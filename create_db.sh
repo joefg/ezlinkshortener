093c784bb48c3190e7c0ee12ecd3f7418ca1cde9
JFG <joefullergray@gmail.com>
2022-06-06 22:31:24 +0100

    Added CLI database management interface

--- create_db.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir data;
-cat sql/base.sql | sqlite3 data/service.db;
--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 import os
+import argparse
+import logging
+
 import sqlite3
 
 SQL_LOCATION = 'sql/'
@@ -10,13 +13,21 @@ def connect(location):
             d[col[0].lower()] = row[idx]
         return d
 
-    conn = sqlite3.connect(os.path.join(location))
-    conn.row_factory = factory
-    cur = conn.cursor()
+    if os.path.exists(os.path.join(location)):
+        conn = sqlite3.connect(os.path.join(location))
+        conn.row_factory = factory
+        cur = conn.cursor()
+    else:
+        raise Exception("Database does not exist. No connection created.")
 
     return conn, cur
 
 def create(location):
+    if os.path.exists(os.path.join(location)):
+        raise Exception("Database already exists. Aborting.")
+
+    open(os.path.join(location), 'a').close()
+
     conn, cur = connect(os.path.join(location))
 
     with open(os.path.join(SQL_LOCATION, 'base.sql'), 'r') as base:
@@ -24,3 +35,57 @@ def create(location):
 
     conn.commit()
     conn.close()
+
+def update(location):
+    raise NotImplementedError("Not implemented yet.")
+    conn, cur = connect(os.path.join(location))
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Database Manager'
+    )
+
+    parser.add_argument(
+        'path',
+        nargs=1,
+        help='Location of data store'
+    )
+
+    parser.add_argument(
+        '--verbose', '-v',
+        action='store_true',
+        help='Increase output verbosity'
+    )
+
+    parser.add_argument(
+        '--create', '-c',
+        action='store_true',
+        help='Create new database'
+    )
+
+    parser.add_argument(
+        '--update', '-u',
+        action='store_true',
+        help='Update existing database'
+    )
+
+    args = vars(parser.parse_args())
+
+    if args['verbose']:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARN)
+
+    logging.debug("Arguments:" + str(args))
+
+    path = args['path'][0]
+
+    if args['create']:
+        create(os.path.join(path))
+        logging.info(f"Created new database at {path}")
+
+    if args['update']:
+        update(os.path.join(path))
